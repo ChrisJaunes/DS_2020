262109ce14e71197db7efc32976783d3e5121df9
hjj1871984569 <hjj1871984569@163.com>
2020-03-20 02:49:26 +0800

    update

--- DS_2020_GUI/DS_2020_GUI/MainWindow.cpp
+++ DS_2020_GUI/DS_2020_GUI/MainWindow.cpp
@@ -35,17 +35,19 @@ void MainWindow::on_btn_search_clicked() {
 		qDebug() << "parse done" << '\n';
 	}
 
-    QString parameter = ui->tet_inputParameter->toPlainText();
-    if (ui->rbn_F1_Author->isChecked()) show_ArticleOfAuthor(parameter);
-    else;
+	QString parameter = ui->tet_inputParameter->toPlainText();
+	if (ui->rbn_F1_Author->isChecked()) {
+		(new Author_Article_Widget(parameter))->show();
+	}
+	else;
 }
 
 void MainWindow::show_ArticleOfAuthor(QString parameter) {
 	qDebug() << "show_ArticleOfAuthor" << parameter << '\n';
-	(new Author_Article_Widget())->show();
+	//(new Author_Article_Widget())->show();
 }
 
 void MainWindow::show_DetailOfArticle(QString parameter) {
 	qDebug() << "show_DetailOfArticle" << parameter << '\n';
-	(new Article_Detail_Widget())->show();
+	//(new Article_Detail_Widget())->show();
 }
