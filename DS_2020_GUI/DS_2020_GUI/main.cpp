5563714796d07ae1da91c81747f09132657378a9
Q4n <1677609544@qq.com>
2020-03-29 01:00:49 +0800

    GUI导入文件

--- DS_2020_GUI/DS_2020_GUI/main.cpp
+++ DS_2020_GUI/DS_2020_GUI/main.cpp
@@ -4,10 +4,13 @@
 
 #include "pch.h"
 #include "test.h"
+#include <Solver.h>
+#include <CommUtils.h>
 
 int main(int argc, char *argv[])
 {
 	FST::create_FST();
+	F3Solver::ImportFromFile(L"D:\\Code\\ds_hw\\new\\new1\\DS_2020\\Database\\F3\\F3_articles.txt");
 
 	QApplication a(argc, argv);
 	MainWindow w;
